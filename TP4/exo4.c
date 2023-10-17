1ee30a877f13d830b87214babed95adde558a407
AMARA WALID <walid.amara@heckel02.u-pem.fr>
2023-10-17 09:26:34 +0200

    Ajout TP4

--- TP4/exo4.c
+++ TP4/exo4.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
-int main(int argc, char const *argv[]) {
+void somme(int tab[], int tab2[], int s[], int taille) {
+    for (int i = 0; i < taille; i++) {
+        s[i] = tab[i] + tab2[i];
+    }
+}
 
+int main(int argc, char const *argv[]) {
+    int tab[] = {1, 2, 3, 4, 5};
+    int tab2[] = {1, 2, 3, 4, 5};
+    int s[];
+    somme(tab, tab2, s, 5);
     return 0;
 }
